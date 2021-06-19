b7d8fbf5df84e290f3ca7a918ecca9cfca1602a3
Miguel hernandez Liebano <mhliebano@gmail.com>
2021-06-18 21:34:48 -0400

    minir changes

--- example/test.dart
+++ example/test.dart
@@ -3,15 +3,18 @@ import 'package:dart_express/dartExpress.dart';
 
 //Abstract Calss inhereted from RouteList
 abstract class Test extends RoutesList {
-  //Impmente a mtehod to expose routes
+  //Implement a method to expose routes
   static Map<String, List<Route>> getRoute() {
     //Enpoint public
-    RoutesList.useGET(Route(
+    RoutesList.useGET(
+      Route(
         route: "/api/route/",
         function: (Request req, HttpResponse res) {
           res.statusCode = HttpStatus.ok;
           res.write("<h1>From Class Test</h1>");
-        }));
+        },
+      ),
+    );
     //Endpoint public
     RoutesList.useGET(
       Route(
