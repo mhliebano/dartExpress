71ce7ef810d7cfef35186da8ce241ccd60ce7694
Miguel Hernandez Liebano <mhliebano@gmail.com>
2022-09-18 22:15:12 -0400

    ejemplo de render

---
+++ example/apiRenderFile/server_render.dart
@@ -0,0 +1,35 @@
+import 'package:dart_express/dartExpress.dart';
+
+void main(List<String> args) {
+  try {
+    ConfigServer config = ConfigServer();
+    DartExpress server = DartExpress(conf: config);
+    server.useCors(true);
+    server.useSecurity(true, secretFrase: "MyT0k3n!Secret");
+
+    //server.useHTTPS(true, config: ConfigHttps(chain: "chain", key: "key"));
+    server.route(
+      Route(
+        verb: routeVerb.GET,
+        path: '/api/test/single/:action/',
+        callback: (IncomingRequest request) {
+          Map<String, dynamic> dataSegment = request.segmentsData;
+          String alert = "<div style='color:red'>No actions</div>";
+          late String action;
+          if (dataSegment["action"] == "1") {
+            alert = "<div style='color:red;background:green'>1 actions</div>";
+            action = "0";
+          } else {
+            action = "1";
+          }
+          request
+              .renderFile("/test.html", {"name": "Miguel", "company": "MMSytems", "message": alert, "action": action});
+        },
+      ),
+    );
+
+    server.run();
+  } catch (e) {
+    print(e);
+  }
+}
